5d856728481060592843688bf4eb5e0991740019
shakked <zshakked@stevens.edu>
2017-10-09 14:11:51 -0400

    Created Xcode Project

---
+++ SquareMeals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SquareMeals
+//
+//  Created by Zachary Shakked on 10/9/17.
+//  Copyright © 2017 Shakd, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
