eb36d8d6b034ea073f68b55725551ea892371493
shakked <zshakked@stevens.edu>
2017-10-18 21:35:02 -0400

    Created CloudKit error enum

--- SquareMeals/Model/CloudKit/CKError.swift
+++ SquareMeals/Model/CloudKit/CKError.swift
@@ -6,4 +6,63 @@
 //  Copyright © 2017 Shakd, LLC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import CloudKit
+
+public enum CKError: AlertableError {
+    
+    case networkUnreachable
+    case couldntDetermine
+    case cloudKitDisabled
+    case cloudKitRestricted
+    
+    init?(accountStatus: CKAccountStatus) {
+        switch accountStatus {
+        case .available:
+            return nil
+        case .couldNotDetermine:
+            self = .couldntDetermine
+        case .noAccount:
+            self = .cloudKitDisabled
+        case .restricted:
+            self = .cloudKitRestricted
+        }
+    }
+    
+    init?(error: Error?) {
+        guard let error = error else {
+            return nil
+        }
+        debugPrint(error)
+        self = .networkUnreachable
+    }
+    
+    public var title: String {
+        switch self {
+        case .networkUnreachable:
+            return "Network Unreachable"
+        case .cloudKitRestricted:
+            return "iCloud Restricted"
+        case .couldntDetermine:
+            return "Couldn't Determine iCloud Status"
+        case .cloudKitDisabled:
+            return "iCloud Disabled"
+        }
+    }
+    
+    public var message: String {
+        switch self {
+        case .networkUnreachable:
+            return "We cannot communicate with the network. Please try again."
+        case .cloudKitRestricted:
+            return "Your iCloud is parentally restricted from using our app. Please contact your parent or guardian to fix this status."
+        case .couldntDetermine:
+            return "We could not determine your iCloud status. Please try again."
+        case .cloudKitDisabled:
+            return "You must be logged into iCloud and have iCloud Drive enabled for 'Captions for Instagram' to use this application. Please open iCloud settings and fix this, then retry our app."
+        }
+    }
+    
+    
+}
+
