64f449c0df8d828b0ce25234e15c051c0199e1d8
Gregory Johnson <gsoccer111@gmail.com>
2017-10-24 15:02:37 -0400

    Meal design
    FInished meal design including table cells

---
+++ SquareMeals/View/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  SquareMeals
+//
+//  Created by Gregory Johnson on 10/24/17.
+//  Copyright © 2017 Shakd, LLC. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
